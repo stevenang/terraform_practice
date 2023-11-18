db338672c3da5eb44b6ae84ddbb0920a0afaeb60
Steven Ang <ang.steve@icloud.com>
2023-11-18 16:49:01 -0500

    Add DLQ Configuration

--- modules/sqs/variables.tf
+++ modules/sqs/variables.tf
@@ -7,3 +7,18 @@ variable "fifo_queue" {
   type    = bool
   default = false
 }
+
+variable "dlq_arn" {
+  type = string
+  default = ""
+}
+
+variable "is_dlq" {
+  type    = bool
+  default = false
+}
+
+variable "max_receive_count" {
+  type    = number
+  default = 5
+}
